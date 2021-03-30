fb75c28d3243494a69073d9d353ab142cec08563
Evgen Kolenkov <kolenkoff@icloud.com>
2021-03-30 09:26:58 +0300

    Initial Commit

---
+++ OnlineCatalogueMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineCatalogueMVP
+//
+//  Created by Evgeniy Kolenkov on 30.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
